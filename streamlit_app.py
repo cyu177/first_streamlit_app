f595b0449ec0ecf7ea9cf26552898063c07a3dd2
cyu177 <136513038+cyu177@users.noreply.github.com>
2023-07-13 18:33:37 -0400

    try allow user to add to fruit list again

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,5 @@ streamlit.header("This fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # allow the end user to add a fruit to the list
-add_my_fruit = 'jackfruit'
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write ('Thanks for adding ', add_my_fruit)
